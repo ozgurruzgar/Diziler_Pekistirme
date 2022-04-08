b3b7a755ea60dfe34d160fc96bf65ba787ea153a
Asus <Asus@ASUS-PC>
2022-04-08 23:11:58 +0300

    Add project files.

---
+++ Diziler_Pekistirme/Form1.Designer.cs
@@ -0,0 +1,374 @@
+﻿
+namespace Diziler_Pekistirme
+{
+    partial class lbl_Ad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewGroup listViewGroup19 = new System.Windows.Forms.ListViewGroup("Ad", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup20 = new System.Windows.Forms.ListViewGroup("Soyad", System.Windows.Forms.HorizontalAlignment.Center);
+            System.Windows.Forms.ListViewGroup listViewGroup21 = new System.Windows.Forms.ListViewGroup("ListViewGroup", System.Windows.Forms.HorizontalAlignment.Left);
+            this.lblAd = new System.Windows.Forms.Label();
+            this.lbl_Soyad = new System.Windows.Forms.Label();
+            this.lbl_Evtel = new System.Windows.Forms.Label();
+            this.lbl_Ceptel = new System.Windows.Forms.Label();
+            this.lbl_EvAdres = new System.Windows.Forms.Label();
+            this.lbl_İsAdres = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.Btn_Yenikyt = new System.Windows.Forms.Button();
+            this.Btn_Kaydet = new System.Windows.Forms.Button();
+            this.Btn_Sil = new System.Windows.Forms.Button();
+            this.Btn_Düzenle = new System.Windows.Forms.Button();
+            this.Btn_Arama = new System.Windows.Forms.Button();
+            this.Btn_Listele = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Ad = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Soyad = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Cep_Telefonu = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // lblAd
+            // 
+            this.lblAd.AutoSize = true;
+            this.lblAd.Location = new System.Drawing.Point(83, 26);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(43, 23);
+            this.lblAd.TabIndex = 0;
+            this.lblAd.Text = "Ad :";
+            // 
+            // lbl_Soyad
+            // 
+            this.lbl_Soyad.AutoSize = true;
+            this.lbl_Soyad.Location = new System.Drawing.Point(53, 60);
+            this.lbl_Soyad.Name = "lbl_Soyad";
+            this.lbl_Soyad.Size = new System.Drawing.Size(73, 23);
+            this.lbl_Soyad.TabIndex = 1;
+            this.lbl_Soyad.Text = "Soyad :";
+            // 
+            // lbl_Evtel
+            // 
+            this.lbl_Evtel.AutoSize = true;
+            this.lbl_Evtel.Location = new System.Drawing.Point(10, 98);
+            this.lbl_Evtel.Name = "lbl_Evtel";
+            this.lbl_Evtel.Size = new System.Drawing.Size(116, 23);
+            this.lbl_Evtel.TabIndex = 2;
+            this.lbl_Evtel.Text = "Ev Telefonu :";
+            // 
+            // lbl_Ceptel
+            // 
+            this.lbl_Ceptel.AutoSize = true;
+            this.lbl_Ceptel.Location = new System.Drawing.Point(-1, 135);
+            this.lbl_Ceptel.Name = "lbl_Ceptel";
+            this.lbl_Ceptel.Size = new System.Drawing.Size(127, 23);
+            this.lbl_Ceptel.TabIndex = 3;
+            this.lbl_Ceptel.Text = "Cep Telefonu :";
+            // 
+            // lbl_EvAdres
+            // 
+            this.lbl_EvAdres.AutoSize = true;
+            this.lbl_EvAdres.Location = new System.Drawing.Point(313, 19);
+            this.lbl_EvAdres.Name = "lbl_EvAdres";
+            this.lbl_EvAdres.Size = new System.Drawing.Size(100, 23);
+            this.lbl_EvAdres.TabIndex = 4;
+            this.lbl_EvAdres.Text = "Ev Adresi : ";
+            // 
+            // lbl_İsAdres
+            // 
+            this.lbl_İsAdres.AutoSize = true;
+            this.lbl_İsAdres.Location = new System.Drawing.Point(313, 98);
+            this.lbl_İsAdres.Name = "lbl_İsAdres";
+            this.lbl_İsAdres.Size = new System.Drawing.Size(89, 23);
+            this.lbl_İsAdres.TabIndex = 5;
+            this.lbl_İsAdres.Text = "İş Adresi :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(132, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 31);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(132, 57);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(169, 31);
+            this.textBox2.TabIndex = 7;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(132, 95);
+            this.maskedTextBox1.Mask = "00000000000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(169, 31);
+            this.maskedTextBox1.TabIndex = 8;
+            this.maskedTextBox1.ValidatingType = typeof(int);
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(132, 132);
+            this.maskedTextBox2.Mask = "00000000000";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(169, 31);
+            this.maskedTextBox2.TabIndex = 9;
+            this.maskedTextBox2.ValidatingType = typeof(int);
+            // 
+            // Btn_Yenikyt
+            // 
+            this.Btn_Yenikyt.Location = new System.Drawing.Point(33, 176);
+            this.Btn_Yenikyt.Name = "Btn_Yenikyt";
+            this.Btn_Yenikyt.Size = new System.Drawing.Size(104, 31);
+            this.Btn_Yenikyt.TabIndex = 10;
+            this.Btn_Yenikyt.Text = "Yeni Kayıt";
+            this.Btn_Yenikyt.UseVisualStyleBackColor = true;
+            this.Btn_Yenikyt.Click += new System.EventHandler(this.Btn_Yenikyt_Click);
+            // 
+            // Btn_Kaydet
+            // 
+            this.Btn_Kaydet.Location = new System.Drawing.Point(33, 213);
+            this.Btn_Kaydet.Name = "Btn_Kaydet";
+            this.Btn_Kaydet.Size = new System.Drawing.Size(104, 31);
+            this.Btn_Kaydet.TabIndex = 11;
+            this.Btn_Kaydet.Text = "Kaydet";
+            this.Btn_Kaydet.UseVisualStyleBackColor = true;
+            this.Btn_Kaydet.Click += new System.EventHandler(this.Btn_Kaydet_Click);
+            // 
+            // Btn_Sil
+            // 
+            this.Btn_Sil.Location = new System.Drawing.Point(33, 250);
+            this.Btn_Sil.Name = "Btn_Sil";
+            this.Btn_Sil.Size = new System.Drawing.Size(104, 31);
+            this.Btn_Sil.TabIndex = 12;
+            this.Btn_Sil.Text = "Sil";
+            this.Btn_Sil.UseVisualStyleBackColor = true;
+            this.Btn_Sil.Click += new System.EventHandler(this.Btn_Sil_Click);
+            // 
+            // Btn_Düzenle
+            // 
+            this.Btn_Düzenle.Location = new System.Drawing.Point(33, 287);
+            this.Btn_Düzenle.Name = "Btn_Düzenle";
+            this.Btn_Düzenle.Size = new System.Drawing.Size(104, 31);
+            this.Btn_Düzenle.TabIndex = 13;
+            this.Btn_Düzenle.Text = "Düzenle";
+            this.Btn_Düzenle.UseVisualStyleBackColor = true;
+            this.Btn_Düzenle.Click += new System.EventHandler(this.Btn_Düzenle_Click);
+            // 
+            // Btn_Arama
+            // 
+            this.Btn_Arama.Location = new System.Drawing.Point(33, 324);
+            this.Btn_Arama.Name = "Btn_Arama";
+            this.Btn_Arama.Size = new System.Drawing.Size(104, 31);
+            this.Btn_Arama.TabIndex = 14;
+            this.Btn_Arama.Text = "Arama";
+            this.Btn_Arama.UseVisualStyleBackColor = true;
+            this.Btn_Arama.Click += new System.EventHandler(this.Btn_Arama_Click);
+            // 
+            // Btn_Listele
+            // 
+            this.Btn_Listele.Location = new System.Drawing.Point(33, 361);
+            this.Btn_Listele.Name = "Btn_Listele";
+            this.Btn_Listele.Size = new System.Drawing.Size(104, 31);
+            this.Btn_Listele.TabIndex = 15;
+            this.Btn_Listele.Text = "Listele";
+            this.Btn_Listele.UseVisualStyleBackColor = true;
+            this.Btn_Listele.Click += new System.EventHandler(this.Btn_Listele_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(154, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 31);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "İlk Kayıt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(264, 176);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 31);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Sonraki";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(377, 176);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 31);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Önceki";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(487, 176);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(104, 31);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Son Kayıt";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Ad,
+            this.Soyad,
+            this.Cep_Telefonu});
+            this.listView1.FullRowSelect = true;
+            listViewGroup19.Header = "Ad";
+            listViewGroup19.Name = "Ad";
+            listViewGroup20.Header = "Soyad";
+            listViewGroup20.HeaderAlignment = System.Windows.Forms.HorizontalAlignment.Center;
+            listViewGroup20.Name = "Soyad";
+            listViewGroup21.Header = "ListViewGroup";
+            listViewGroup21.Name = "listViewGroup2";
+            this.listView1.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
+            listViewGroup19,
+            listViewGroup20,
+            listViewGroup21});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(154, 213);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(437, 195);
+            this.listView1.TabIndex = 20;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Ad
+            // 
+            this.Ad.Text = "Ad";
+            this.Ad.Width = 96;
+            // 
+            // Soyad
+            // 
+            this.Soyad.Text = "Soyad";
+            this.Soyad.Width = 122;
+            // 
+            // Cep_Telefonu
+            // 
+            this.Cep_Telefonu.Text = "Cep Telefonu";
+            this.Cep_Telefonu.Width = 177;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(408, 16);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(183, 67);
+            this.richTextBox1.TabIndex = 21;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(408, 95);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(183, 67);
+            this.richTextBox2.TabIndex = 22;
+            this.richTextBox2.Text = "";
+            // 
+            // lbl_Ad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 420);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Btn_Listele);
+            this.Controls.Add(this.Btn_Arama);
+            this.Controls.Add(this.Btn_Düzenle);
+            this.Controls.Add(this.Btn_Sil);
+            this.Controls.Add(this.Btn_Kaydet);
+            this.Controls.Add(this.Btn_Yenikyt);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbl_İsAdres);
+            this.Controls.Add(this.lbl_EvAdres);
+            this.Controls.Add(this.lbl_Ceptel);
+            this.Controls.Add(this.lbl_Evtel);
+            this.Controls.Add(this.lbl_Soyad);
+            this.Controls.Add(this.lblAd);
+            this.Font = new System.Drawing.Font("Corbel", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "lbl_Ad";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.lbl_Ad_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAd;
+        private System.Windows.Forms.Label lbl_Soyad;
+        private System.Windows.Forms.Label lbl_Evtel;
+        private System.Windows.Forms.Label lbl_Ceptel;
+        private System.Windows.Forms.Label lbl_EvAdres;
+        private System.Windows.Forms.Label lbl_İsAdres;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.Button Btn_Yenikyt;
+        private System.Windows.Forms.Button Btn_Kaydet;
+        private System.Windows.Forms.Button Btn_Sil;
+        private System.Windows.Forms.Button Btn_Düzenle;
+        private System.Windows.Forms.Button Btn_Arama;
+        private System.Windows.Forms.Button Btn_Listele;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Ad;
+        private System.Windows.Forms.ColumnHeader Soyad;
+        private System.Windows.Forms.ColumnHeader Cep_Telefonu;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ Diziler_Pekistirme/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Diziler_Pekistirme
+{
+    public partial class lbl_Ad : Form
+    {
+        public lbl_Ad()
+        {
+            InitializeComponent();
+        }
+        //Kullanıcağımız Dizileri ve Değişkenleri Tanımlıyoruz.
+        string[] ad = new string[1];
+        string[] soyad = new string[1];
+        string[] ceptel = new string[1];
+        string[] evtel = new string[1];
+        string[] evadres = new string[1];
+        string[] isadres = new string[1];
+        int sıra = 0;
+        int kacinci = 0;
+
+        private void lbl_Ad_Load(object sender, EventArgs e)
+        { }
+
+        private void Btn_Kaydet_Click(object sender, EventArgs e)
+        {
+            ad[sıra] = textBox1.Text;
+            soyad[sıra] = textBox2.Text;
+            ceptel[sıra] = maskedTextBox2.Text;
+            evtel[sıra] = maskedTextBox1.Text;
+            evadres[sıra] = richTextBox1.Text;
+            isadres[sıra] = richTextBox2.Text;
+            sıra++;
+            Array.Resize(ref ad, sıra + 1);
+            Array.Resize(ref soyad, sıra + 1);
+            Array.Resize(ref ceptel, sıra + 1);
+            Array.Resize(ref evtel, sıra + 1);
+            Array.Resize(ref evadres, sıra + 1);
+            Array.Resize(ref isadres, sıra + 1);
+        }
+        private void Btn_Listele_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            int eleman = ad.Length;
+            for(int i =0;i<eleman;i++)
+            {
+                ListViewItem kayit = new ListViewItem();
+                kayit.Text = ad[i];
+                kayit.SubItems.Add(soyad[i]);
+                kayit.SubItems.Add(ceptel[i]);
+                listView1.Items.Add(kayit);
+            }
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            //ilk Kayıt Butonu
+            textBox1.Text = ad[0];
+            textBox2.Text = soyad[0];
+            maskedTextBox1.Text =evtel[0];
+            maskedTextBox2.Text =ceptel[0];
+            richTextBox1.Text = evadres[0];
+            richTextBox2.Text = isadres[0];
+            kacinci = 0;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Son Kayıt Butonu
+            int a = ad.Length - 2;
+            textBox1.Text = ad[a];
+            textBox2.Text = soyad[a];
+            maskedTextBox1.Text = evtel[a];
+            maskedTextBox2.Text = ceptel[a];
+            richTextBox1.Text = evadres[a];
+            richTextBox2.Text = isadres[a];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Sonraki Butonu
+            if(kacinci < ad.Length-2)
+            {
+                textBox1.Text = ad[kacinci];
+                textBox2.Text = soyad[kacinci];
+                maskedTextBox1.Text = evtel[kacinci];
+                maskedTextBox2.Text = ceptel[kacinci];
+                richTextBox1.Text = evadres[kacinci];
+                richTextBox2.Text = isadres[kacinci];
+            }
+            else
+            {
+                MessageBox.Show("Son Kayıttasınız.","Bilgi",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Önceki Butonu
+            if (kacinci != 0)
+            {
+                textBox1.Text = ad[kacinci];
+                textBox2.Text = soyad[kacinci];
+                maskedTextBox1.Text = evtel[kacinci];
+                maskedTextBox2.Text = ceptel[kacinci];
+                richTextBox1.Text = evadres[kacinci];
+                richTextBox2.Text = isadres[kacinci];
+            }
+            else
+            {
+                MessageBox.Show("İlk Kayıttasınız.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void Btn_Arama_Click(object sender, EventArgs e)
+        {
+            //Arama Butonu
+            string aranacak = Microsoft.VisualBasic.Interaction.InputBox("Arananın Adı :","Arama :","",0,0);
+            int a = Array.IndexOf(ad, aranacak);
+            textBox1.Text = ad[a];
+            textBox2.Text = soyad[a];
+            maskedTextBox1.Text = evtel[a];
+            maskedTextBox2.Text = ceptel[a];
+            richTextBox1.Text = evadres[a];
+            richTextBox2.Text = isadres[a];
+        }
+
+        private void Btn_Düzenle_Click(object sender, EventArgs e)
+        {
+            //Düzenle Butonu
+            ad[kacinci] = textBox1.Text;
+            soyad[kacinci] = textBox2.Text;
+            evtel[kacinci] = maskedTextBox1.Text;
+            ceptel[kacinci] = maskedTextBox2.Text;
+            evadres[kacinci] = richTextBox1.Text;
+            isadres[kacinci] = richTextBox2.Text;
+        }
+
+        private void Btn_Yenikyt_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            maskedTextBox1.Text = "";
+            maskedTextBox2.Text = "";
+            richTextBox1.Text = "";
+            richTextBox2.Text = "";
+        }
+
+        private void Btn_Sil_Click(object sender, EventArgs e)
+        {
+            Array.Clear(ad,kacinci,1);
+            Array.Clear(soyad, kacinci,1);
+            Array.Clear(evtel, kacinci,1);
+            Array.Clear(ceptel, kacinci,1);
+            Array.Clear(evadres, kacinci,1);
+            Array.Clear(isadres, kacinci,1);
+        }
+    } } 
